a2cd28ece7b4a7a68ab90be24bb4fab972d18f46
yiyupan <yiyupan@umich.edu>
2017-08-24 15:11:09 -0400

    v2.0

--- newsdatadb.py
+++ newsdatadb.py
@@ -1,84 +1,92 @@
-# Database code for the DB News
+#!/usr/bin/env python3
 
 import psycopg2
 
 DBNAME = "news"
 
 
-def get_top_articles():
-    """Return top 3 articles"""
+def db_connect():
+    """ Creates and returns a connection to the database defined by DBNAME,
+        as well as a cursor for the database.
+
+        Returns:
+            db, c - a tuple. The first element is a connection to the database.
+                    The second element is a cursor for the database.
+    """
+
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-
-    c.execute(
-        "select title, count(log.path) as views " +
-        "from articles, log " +
-        "where log.path = ('/article/' || articles.slug) " +
-        "group by title " +
-        "order by views desc " +
-        "limit 3")
-    top_articles = c.fetchall()
-    db.close()
-    return top_articles
+    db_c = (db, c)
+    return db_c
 
 
-def get_top_authors():
-    """Return authors ranking by views"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    # Create view slug_author
-    '''c.execute("create view slug_author as " +
-    "select slug, name from articles join authors " +
-    "on articles.author = authors.id")
-    '''
-    c.execute(
-        "select name, count(log.path) as views " +
-        "from slug_author, log " +
-        "where log.path = ('/article/' || slug_author.slug) " +
-        "group by name " +
-        "order by views desc ")
-    top_authors = c.fetchall()
-    db.close()
-    return top_authors
+def execute_query(query):
+    """execute_query takes an SQL query as a parameter.
+        Executes the query and returns the results as a list of tuples.
+       args:
+           query - an SQL query statement to be executed.
+
+       returns:
+           A list of tuples containing the results of the query.
+    """
+
+    try:
+        db_c = db_connect()
+        db = db_c[0]
+        c = db_c[1]
+        c.execute(query)
+        res = c.fetchall()
+        db.close()
+        return res
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
-def get_error_percent():
-    """Return days having more than 1% of requests lead to errors"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    # Create view requests
-    '''c.execute("create view requests as " +
-    "select date(time), count(*) as total " +
-    "from log " +
-    "group by date(time) ")
-    # Create view errors
-    c.execute("create view errors as " +
-    "select date(time), count(status) as num, status " +
-    "from log " +
-    "where status != '200 OK' " +
-    "group by date(time) " +
-    "order by date(time)")
-    '''
-    c.execute(
-        "select errors.date, num * 100.0 / (total) as percentage, status " +
-        "from requests, errors " +
-        "where requests.date = errors.date and  num * 100.0 / (total) > 1 " +
-        "group by errors.date, percentage, status")
-    err_prpercent = c.fetchall()
-    db.close()
-    return err_prpercent
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = """select title, count(log.path) as views
+        from articles, log
+        where log.path = ('/article/' || articles.slug)
+        group by title
+        order by views desc
+        limit 3"""
+    results = execute_query(query)
+    print("\nTop Three Articles: ")
+    print("----------------------")
+    for title, views in results:
+        print('\"{}\" --- {} views'.format(title, views))
 
-print("Top Three Articles: ")
-articles = get_top_articles()
-for item in articles:
-    print(item[0] + " --- " + str(item[1]) + " views")
 
-print("\nAuthor Ranking: ")
-authors = get_top_authors()
-for item in authors:
-    print(item[0] + " --- " + str(item[1]) + " views")
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = """select name, count(log.path) as views
+        from slug_author, log
+        where log.path = ('/article/' || slug_author.slug)
+        group by name
+        order by views desc"""
+    results = execute_query(query)
+    print("\nAuthor Ranking: ")
+    print("----------------------")
+    for name, views in results:
+        print('\"{}\" --- {} views'.format(name, views))
 
-print("\nDate had more than 1% of requests lead to errors: ")
-e = get_error_percent()
-for item in e:
-    print(str(item[0]) + " --- " + '%.2f' % item[1] + "% " + item[2])
+
+def print_errors_over_one():
+    """Prints out the days where more than 1%
+    of logged access requests were errors."""
+    query = """select errors.date, num * 100.0 / (total) as percentage, status
+        from requests, errors
+        where requests.date = errors.date and  num * 100.0 / (total) > 1
+        group by errors.date, percentage, status"""
+    results = execute_query(query)
+    print("\nDate had more than 1% of requests lead to errors: ")
+    print("----------------------")
+    for item in results:
+        print('{0:%B %d, %Y} -- {1:.2f}% {2}'.format(
+            item[0], item[1], item[2]))
+
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
